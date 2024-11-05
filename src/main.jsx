d6e31cde1dd0f89145717f1c7fc50e9abd70fa2c
Jou Okuyama <ojoxux@gmail.com>
2024-11-05 19:28:52 +0900

    style: ChakraUIを適用するための更新

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import './index.css';
+import App from './App.jsx';
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'transparent',
+      },
+    },
+  },
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
