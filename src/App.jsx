a528084c075a27704d5b5a708a4c643c925ac53e
Jou Okuyama <ojoxux@gmail.com>
2024-11-05 19:26:49 +0900

    feat: 誤ったパスの変更 & 背景のグラデーションの適用

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import {
   Navigate,
 } from 'react-router-dom';
 import './App.css';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
+import SignIn from './components/Auth/SignIn';
+import SignUp from './components/Auth/SignUp';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/signin" />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <>
+      <div className="gradient-overlay" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signin" />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
