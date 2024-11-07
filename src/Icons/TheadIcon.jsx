558d48e25423da2a97c80cc277207f294ac8f4e2
Jou Okuyama <ojoxux@gmail.com>
2024-11-07 16:33:38 +0900

    feat: サイドバーのアイコンファイルの追加

---
+++ src/Icons/TheadIcon.jsx
@@ -0,0 +1,20 @@
+const ThreadIcon = () => (
+  <svg
+    className="w-[48px] h-[48px] text-gray-800 dark:text-white"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeWidth="1.9"
+      d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-3 5h3m-6 0h.01M12 16h3m-6 0h.01M10 3v4h4V3h-4Z"
+    />
+  </svg>
+);
+
+export default ThreadIcon;
