5a67b134341e690039024046f4e8bdc0a6ca1b47
Jou Okuyama <ojoxux@gmail.com>
2024-11-07 16:35:00 +0900

    feat: ルーティングの追加 & ヘッダーの表示設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import './App.css';
+import Header from './components/ui/Header';
 import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Thread from './components/Thread';
 
 function App() {
+  const location = useLocation();
+  const hideHeaderRoutes = ['/signin', '/signup'];
+
   return (
     <>
       <div className="gradient-overlay" />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/signin" />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </Router>
+      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/thread" element={<Thread />} />
+      </Routes>
     </>
   );
 }
