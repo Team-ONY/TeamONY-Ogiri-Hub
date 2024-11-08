b9d3961eb7ef9fb6ee783e171781499b00b9fe4a
Jou Okuyama <ojoxux@gmail.com>
2024-11-08 19:15:18 +0900

    style: アイコンが白にならない問題のため、strokeをwhiteに変更

--- src/Icons/TheadIcon.jsx
+++ src/Icons/TheadIcon.jsx
@@ -9,7 +9,7 @@ const ThreadIcon = () => (
     viewBox="0 0 24 24"
   >
     <path
-      stroke="currentColor"
+      stroke="white"
       strokeLinecap="round"
       strokeWidth="1.9"
       d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-3 5h3m-6 0h.01M12 16h3m-6 0h.01M10 3v4h4V3h-4Z"
