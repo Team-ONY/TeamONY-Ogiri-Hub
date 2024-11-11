ced1d4e7ab256e0a231e83f61c0407236776a5f1
Jou Okuyama <ojoxux@gmail.com>
2024-11-11 13:15:02 +0900

    style: スレッド内のコメントの間隔を広げた

--- src/components/ThreadDetail.jsx
+++ src/components/ThreadDetail.jsx
@@ -273,6 +273,7 @@ function ThreadDetail() {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: index * 0.1, duration: 0.3 }}
+            mb={4}
           >
             <Flex
               align="center"
