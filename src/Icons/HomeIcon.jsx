f2808d5f30fe6df355e98932e3aac043dca566a7
Jou Okuyama <ojoxux@gmail.com>
2024-12-02 15:56:11 +0900

    feat: HomeIconコンポーネントの線の色を白に変更

--- src/Icons/HomeIcon.jsx
+++ src/Icons/HomeIcon.jsx
@@ -9,7 +9,7 @@ export const HomeIcon = () => (
     viewBox="0 0 24 24"
   >
     <path
-      stroke="currentColor"
+      stroke="white"
       strokeLinecap="round"
       strokeWidth="1.9"
       d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"
