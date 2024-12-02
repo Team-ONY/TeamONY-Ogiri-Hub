627f075d6612c8471d125af4fc616dadbd5189ee
Jou Okuyama <ojoxux@gmail.com>
2024-12-02 15:55:29 +0900

    feat: PlusIconコンポーネントの追加

---
+++ src/Icons/PlusIcon.jsx
@@ -0,0 +1,20 @@
+const PlusIcon = () => (
+  <svg
+    className="w-[48px] h-[48px] text-gray-800 dark:text-white"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="white"
+      strokeLinecap="round"
+      strokeWidth="1.9"
+      d="M12 4v16m8-8H4"
+    />
+  </svg>
+);
+
+export default PlusIcon;
