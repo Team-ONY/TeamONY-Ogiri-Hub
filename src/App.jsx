c532edf88c8a5efa48816a521d7fb3349e323d6b
Jou Okuyama <ojoxux@gmail.com>
2024-12-02 16:01:36 +0900

    refactor: 🛠️ Appコンポーネントをリファクタリングし、新しいLayoutコンポーネントを統合して構造とレスポンシブ性を強化。CreateThreadルートを削除し、ルートアクセス時に/homeへリダイレクトするようナビゲーションを更新。

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import './App.css';
-import Header from './components/UI/Header';
 import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
 import Home from './components/Home/Home';
 import Thread from './components/Thread/Thread';
 import ThreadDetail from './components/Thread/ThreadDetail';
-import CreateThread from './components/Thread/CreateThread';
 import AdminPage from './components/Admin/AdminPage';
 import ProfileMasonry from './components/Profile/ProfileMasonry';
 import ProfileEdit from './components/Profile/ProfileEdit';
@@ -14,80 +12,77 @@ import { AlertProvider } from './context/AlertContext';
 import { AuthProvider } from './context/AuthContext';
 import { PrivateRoute } from './components/Routes/PrivateRoute';
 import { AdminRoute } from './components/Admin/AdminRoute';
+import Layout from './components/UI/Layout';
 
 function App() {
   const location = useLocation();
   const hideHeaderRoutes = ['/signin', '/signup'];
 
   return (
-    <>
-      <AuthProvider>
-        {!hideHeaderRoutes.includes(location.pathname) && <Header />}
-        <AlertProvider>
+    <AuthProvider>
+      <AlertProvider>
+        {!hideHeaderRoutes.includes(location.pathname) ? (
+          <Layout>
+            <Routes>
+              <Route
+                path="/home"
+                element={
+                  <PrivateRoute>
+                    <Home />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/thread"
+                element={
+                  <PrivateRoute>
+                    <Thread />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/thread/:id"
+                element={
+                  <PrivateRoute>
+                    <ThreadDetail />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/admin/:id"
+                element={
+                  <AdminRoute>
+                    <AdminPage />
+                  </AdminRoute>
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <PrivateRoute>
+                    <ProfileMasonry />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/profile/edit"
+                element={
+                  <PrivateRoute>
+                    <ProfileEdit />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </Layout>
+        ) : (
           <Routes>
-            <Route path="/" element={<Navigate to="/signin" />} />
+            <Route path="/" element={<Navigate to="/home" />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route
-              path="/home"
-              element={
-                <PrivateRoute>
-                  <Home />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/thread"
-              element={
-                <PrivateRoute>
-                  <Thread />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/thread/:id"
-              element={
-                <PrivateRoute>
-                  <ThreadDetail />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/create-thread"
-              element={
-                <PrivateRoute>
-                  <CreateThread />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/admin/:id"
-              element={
-                <AdminRoute>
-                  <AdminPage />
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <PrivateRoute>
-                  <ProfileMasonry />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/profile/edit"
-              element={
-                <PrivateRoute>
-                  <ProfileEdit />
-                </PrivateRoute>
-              }
-            />
           </Routes>
-        </AlertProvider>
-      </AuthProvider>
-    </>
+        )}
+      </AlertProvider>
+    </AuthProvider>
   );
 }
 
